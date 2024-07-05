a5924ce71e8a267b099d797aad1b03182c6aaf70
alyanisa <118141876+alyanisa@users.noreply.github.com>
2024-07-05 17:34:42 +0700

    Update game_gunting_batu_kertas.py

--- game_gunting_batu_kertas.py
+++ game_gunting_batu_kertas.py
@@ -1,29 +1,40 @@
+# Permainan Gunting, Batu, Kertas
+
+Kode berikut adalah implementasi permainan "Gunting, Batu, Kertas" menggunakan bahasa pemrograman Python. Anda akan bermain melawan komputer yang memilih secara acak di setiap putaran.
+
+# Mengimpor Modul Random
 import random
 
-a=0
-s=int(input("mau main berapa kali:"))
+# Inisialisasi Variabel
+a = 0
+s = int(input("Mau main berapa kali:")) # Pemain memasukkan ingin berapa kali main
 
-while a<s:
-    a=a+1
+# Loop untuk Setiap Putaran
+while a < s:
+    a = a + 1
 
-    you=input("masukkan pilihan antara gunting batu kertas")
-    choose=["gunting","batu","kertas"]
-    computer=random.choice(choose)
+    # Input Pemain dan Pilihan Komputer
+    you = input("Masukkan pilihan antara gunting, batu, kertas: ")
+    choose = ["gunting", "batu", "kertas"]
+    computer = random.choice(choose)
 
-    if you==computer:
-        print("seri")
-    elif you=="gunting":
-        if computer=="kertas":
-            print("computer=kertas kamu menang!!") 
+    if you == computer:
+        print("Seri") # Logika Permainan (Seri)
+    # Logika Permainan (Pemain memilih Gunting)
+    elif you == "gunting":
+        if computer == "kertas":
+            print("Computer=kertas kamu menang!!")
         else:
-            print("computer=batu kamu kalah!!")
-    elif you=="batu":
-        if computer=="gunting":
-            print("computer=gunting kamu menang!!") 
+            print("Computer=batu kamu kalah!!")
+    # Logika Permainan (Pemain memilih Batu)
+    elif you == "batu":
+        if computer == "gunting":
+            print("Computer=gunting kamu menang!!")
         else:
-            print("computer=kertas kamu kalah!!")
-    elif you=="kertas":
-        if computer=="batu":
-            print("computer=batu kamu menang!!") 
+            print("Computer=kertas kamu kalah!!")
+    # Logika Permainan (Pemain Memilih Kertas)
+    elif you == "kertas":
+        if computer == "batu":
+            print("Computer=batu kamu menang!!")
         else:
-            print("computer=gunting kamu kalah!!")
+            print("Computer=gunting kamu kalah!!")
